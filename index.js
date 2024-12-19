01a82c9529e1408adf522a22f9fea66d6c12a95c
kisesesky <kisesesky19@gmail.com>
2024-12-19 16:44:55 +0900

    feat: modal add

--- index.js
+++ index.js
@@ -1,1 +1,104 @@
----
+this.addEventListener('DOMContentLoaded',()=>{
+    const uploadElement = document.getElementById('upload')
+    const imageElement = document.getElementById('profile-img')
+    const uploadModalElement = document.getElementById('uploadModal')
+    const updateElement = document.getElementById('update-profile-btn')
+    const saveElement = document.getElementById('save')
+    const closeElement = document.getElementById('close')
+    const imagePreviewElement = document.getElementById('imagePreview')
+
+    const modalIdElement = document.getElementById('id')
+    const modalnameElement = document.getElementById('name')
+    const modalWebsiteElement = document.getElementById('website')
+    const modalProfileElement = document.getElementById('profileIdx')
+
+    const profileIdElement = document.getElementById('profile-id')
+    const profileNameElement = document.getElementById('profile-name')
+    const profileWebsiteElement = document.getElementById('profile_website')
+    const profileDescriptionElement = document.getElementById('profile-description')
+
+
+
+
+    function saveImageToLocalStorage(data){
+        localStorage.setItem('image',data)
+        imageElement.src=data;
+    }
+
+    function loadImageToLocalStorage(){
+        const image = localStorage.getItem('image');
+        if(image){
+            imageElement.src = image;
+        }
+    }
+
+    function saveProfileToLocalStorage(){
+        const profileData = {
+            id : modalIdElement.value,
+            name: modalnameElement.value,
+            website: modalWebsiteElement.value,
+            description: modalProfileElement.value
+        };
+        localStorage.setItem('profileData',JSON.stringify(profileData));
+    }
+
+    function loadProfileToLocalStorage(){
+        const profileData = JSON.parse(localStorage.getItem('profileData'));
+        if(profileData) {
+            profileIdElement.textContent = profileData.id;
+            profileNameElement.textContent = profileData.name;
+            profileWebsiteElement.textContent = profileData.website;
+            profileDescriptionElement.textContent = profileData.description;
+        } else{
+            profileIdElement.textContent = ''
+            profileNameElement.textContent = ''
+            profileWebsiteElement.textContent ='' 
+            profileDescriptionElement.textContent = ''
+
+        }
+    }
+
+    saveElement.addEventListener('click',()=>{
+        if(imagePreview.src){
+            saveImageToLocalStorage(imagePreviewElement.src);
+            saveProfileToLocalStorage();            
+            uploadElement.close();
+        }
+    })
+
+    //close클릭 및 esc
+    window.addEventListener('keydown', (e)=>{
+        if(e.key ==='Escape'){
+            uploadModalElement.close();
+        }
+    })
+    closeElement.addEventListener('click',(e)=>{
+        uploadModalElement.close();
+    })
+
+    // 
+    updateElement.addEventListener('click',()=>{
+        loadProfileToLocalStorage();
+        uploadModalElement.showModal();
+    })
+    imagePreviewElement.addEventListener('click',()=>{
+        uploadElement.click();
+    })
+    
+
+    uploadElement.addEventListener('change',(e)=>{
+        const file = e.target.files[0];
+        if(file){
+            const reader = new FileReader()
+            reader.onload = function(e){
+                const imageData = e.target.result;
+                imagePreviewElement.src = imageData;
+            }
+            reader.readAsDataURL(file)
+        }
+    })
+    loadImageToLocalStorage();
+    loadProfileToLocalStorage();
+
+
+})
